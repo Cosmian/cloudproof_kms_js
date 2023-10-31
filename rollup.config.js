48a5a9e06b6e9e0a63c5684b876f2f0882e75d81
ccorsin <celia.corsin@gmail.com>
2023-10-31 10:31:08 +0100

    fix: edit package json file

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs"
 import typescript from "@rollup/plugin-typescript"
-import { wasm } from "@rollup/plugin-wasm"
 
 const production = !process.env.ROLLUP_WATCH
 
@@ -20,34 +19,14 @@ const rolls = (fmt, env) => ({
     entryFileNames: `[name].${fmt === "cjs" ? "cjs" : "js"}`,
     name: "cloudproof_kms_js",
   },
-  external: ["jose", "uuid", "base64-js"],
+  external: ["jose"],
   plugins: [
     commonjs(),
-    // We want to inline our wasm bundle as base64. Not needing browser users
-    // to fetch an additional asset is a boon as there's less room for errors
-    env !== "slim" &&
-      wasm(
-        env === "node"
-          ? { maxFileSize: 0, targetEnv: "node" }
-          : { targetEnv: "auto-inline" },
-      ),
     typescript({
       outDir: outdir(fmt, env),
       rootDir: "src",
       sourceMap: !production,
     }),
-    {
-      name: "copy-pkg",
-
-      // wasm-bindgen outputs a import.meta.url when using the web target.
-      // rollup will either preserve the the statement when outputting an esm,
-      // which will cause webpack < 5 to choke or it will output a
-      // "require('url')", for other output types, causing more choking. Since
-      // we want a downstream developer to either not worry about providing wasm
-      // at all, or forcing them to deal with bundling, we resolve the import to
-      // an empty string. This will error at runtime.
-      resolveImportMeta: () => `""`,
-    },
   ],
 })
 
